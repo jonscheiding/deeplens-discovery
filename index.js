473d9724b5f3e7420ba0fd4a5c88830e6715bf41
Jon Scheiding <jonscheiding@gmail.com>
2018-01-23 16:43:42 -0500

    Updated with --server and --client options

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import { Discovery } from 'udp-discovery';
+import { argv } from 'yargs'
 
 const SERVICE_NAME = "deeplens-discovery";
 
 const discoveryService = new Discovery();
 
-discoveryService.on('available', (name, data, reason) => {
-    console.log('server: available', { name, data, reason });
-    discoveryService.sendEvent('deeplens', {});
-});
+if (!argv.server && !argv.client) {
+    console.error('Please specify --server or --client.');
+    process.exit(1);
+}
 
-discoveryService.announce(SERVICE_NAME, {}, 500, true);
+if(argv.server) {
+    discoveryService.on('available', (name, data, reason) => {
+        console.log('server: available', { name, data, reason });
+        discoveryService.sendEvent('deeplens', {});
+    });
+}
 
-discoveryService.on('MessageBus', (event, data) => {
-    console.log('client: event', { event, data });
-});
+if(argv.client) {
+    discoveryService.announce(SERVICE_NAME, {}, 500, true);
+
+    discoveryService.on('MessageBus', (event, data) => {
+        console.log('client: event', { event, data });
+    });
+}
